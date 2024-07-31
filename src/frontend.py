05c729bdcc2cf662651d9f43dc5672c655399356
Fabian Tomczyk <fabian.tomczyk@gmx.de>
2024-07-31 20:50:19 +0200

    Adding first widgets and placement

--- src/frontend.py
+++ src/frontend.py
@@ -16,6 +16,8 @@ app.option_add("*tearOff", False)
 trans = Translator("../lang")
 trans.set_locale("de")
 
+edge = 1
+edge_left = 1
 #================================================
 # Buttonfunctions
 #================================================
@@ -30,8 +32,24 @@ app.config(menu = menubar)
 menu_options = tk.Menu(menubar)
 menubar.add_cascade(menu = menu_options,
                     label = trans.translate("options"))
+
 menu_options.add_command(label = trans.translate("language"), 
                          command = but_func)
+
+your_throw = tk.Label(text = trans.translate("your_throw"))
+
+dice_frame = tk.Frame(width = 200, height = 550,
+                      borderwidth = 2, relief = "groove")
+
+# Add validation to only allow numbers
+edge_entry = tk.Entry(width = 2, textvariable = edge)
+edge_label = tk.Label(text = trans.translate("edge"))
+
+edge_left = tk.Entry(width = 2, state = "readonly", textvariable = edge_left)
+edge_left_label = tk.Label(text = trans.translate("edge_left"))
+
+history_frame = tk.Frame(width = 200, height = 200,
+                   borderwidth = 2, relief = "groove")
 
 but = tk.Button(master = app,
                 text = trans.translate("throw"),
@@ -41,6 +59,13 @@ but = tk.Button(master = app,
 # Layout
 #================================================
 but.place(relx = 0.5, rely = 0.5, anchor = tk.CENTER)
+your_throw.grid(column = 0, row = 0)
+dice_frame.grid(column = 0, row = 1, rowspan = 8)
+edge_entry.grid(column = 2, row = 1)
+edge_label.grid(column = 3, row = 1)
+edge_left.grid(column = 2, row = 2)
+edge_left_label.grid(column = 3, row = 2)
+#history_frame.grid(column = 2, row = 3)
 
 #================================================
 # mainloop
