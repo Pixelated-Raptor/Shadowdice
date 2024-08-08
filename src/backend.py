dc5a780d9635b81d9305e215d994275afe5e445b
Fabian Tomczyk <fabian.tomczyk@gmx.de>
2024-08-08 16:05:54 +0200

    Started work on backend

---
+++ src/backend.py
@@ -0,0 +1,14 @@
+def bk_throw():
+    print("throw")
+
+def bk_pre_edge():
+    print("pre edge")
+
+def bk_post_edge():
+    print("post edge")
+
+def bk_edge_roll():
+    print("edge_roll")
+
+def bk_reroll_misses():
+    print("reroll misses")
--- src/frontend.py
+++ src/frontend.py
@@ -3,6 +3,7 @@
 #================================================
 import tkinter as tk
 from translate import Translator
+from backend import *
 
 #================================================
 # Setup
@@ -18,7 +19,7 @@ trans.set_locale("de")
 
 edge = ""
 edge_left = ""
-dice_pool = ""
+dice_pool = tk.IntVar(value = 1)
 
 big_font = ("Arial", 16)
 regular_font = ("Arial", 12)
@@ -28,6 +29,21 @@ regular_font = ("Arial", 12)
 def but_func():
     print("Button pressed")
 
+def throw():
+    print("throw " + str(dice_pool.get()) + " dice")
+
+def pre_edge():
+    print("pre edge")
+
+def post_edge():
+    print("post edge")
+
+def edge_roll():
+    print("edge roll")
+    
+def reroll_misses():
+    print("reroll misses")
+    
 #================================================
 # Widgets
 #================================================
@@ -58,24 +74,23 @@ dice_pool_spinbox = tk.Spinbox(from_ = 1, to = 99, increment = 1,
                                width = 2, textvariable = dice_pool, 
                                font = regular_font)
 
-throw_btn = tk.Button(text = trans.translate("throw"), command = but_func,
+throw_btn = tk.Button(text = trans.translate("throw"), command = throw,
                       font = regular_font)
 
-pre_edge_btn = tk.Button(text = trans.translate("pre-edge"), command = but_func,
+pre_edge_btn = tk.Button(text = trans.translate("pre-edge"), command = pre_edge,
                          font = regular_font, width = 12)
-post_edge_btn = tk.Button(text = trans.translate("post-edge"), command = but_func,
+post_edge_btn = tk.Button(text = trans.translate("post-edge"), command = post_edge,
                           font = regular_font, width = 12)
 
-edge_roll_btn = tk.Button(text = trans.translate("edge-roll"), command = but_func,
+edge_roll_btn = tk.Button(text = trans.translate("edge-roll"), command = edge_roll,
                           font = regular_font)
-reroll_misses_btn = tk.Button(text = trans.translate("reroll_misses"), command = but_func,
-                              font = regular_font)
+reroll_misses_btn = tk.Button(text = trans.translate("reroll_misses"), command = reroll_misses,                            font = regular_font)
 
 #================================================
 # Layout
 #================================================
 your_throw.grid(column = 0, row = 0)
-dice_frame.grid(column = 0, row = 1, rowspan = 10)
+dice_frame.grid(column = 0, row = 1, rowspan = 11)
 edge_entry.grid(column = 1, row = 1)
 edge_label.grid(column = 2, row = 1, sticky = "w")
 edge_left.grid(column = 1, row = 2)
